37edab1b5391263da4afa6596195f54487e72333
JoaoVictorTZ <j.zanotto@escola.pr.gov.br>
2025-07-02 20:24:58 -0300

    02/07/2025 (1)

--- script.js
+++ script.js
@@ -6,7 +6,34 @@ function mostraPalavraChave() {
     //alert("Fui clicado!");
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ")
+    const palavras = processaTexto(texto);
 
     campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto(texto) {
+    //let palavras = texto.split(/\s+/);  "retirar espaços"
+    //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
+    let palavras = texto.split("/\P{L}+/u/");  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+
+    const frequencias = contaFrequencias(palavras);
+
+    let ordenadas = Object.keys(frequencias).sort(ordenadaPalavras);
+    function ordenaPalavra(p1, p2){
+        return frequencias[p2] - frequencias[p1];
+    }
+    return ordenadas.slice(0,10);
+}
+
+function contaFrequencias(palavras)
+    let frequencias = [];
+    for ( let i of palavras) {
+        frequencias[i] = 0;
+        for ( let j of palavras) {
+            if( i == j ) {
+                frequencias[i]++;
+            }
+        }
+    }
+    return palavras;
 }
