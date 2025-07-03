91caced9da7118f34a92ab6b1ce1459e8a38ff39
JoaoVictorTZ <j.zanotto@escola.pr.gov.br>
2025-07-02 21:16:54 -0300

    02/07/2025 (3)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import{PALAVRAS_RUINS} from "./palavrasRuins"
+
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
@@ -18,15 +21,15 @@ function processaTexto(texto) {
 
     const frequencias = contaFrequencias(palavras);
 
-    let ordenadas = Object.keys(frequencias).sort(ordenadaPalavras);
-    function ordenaPalavra(p1, p2){
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    function ordenaPalavra(p1, p2) {
         return frequencias[p2] - frequencias[p1];
     }
     return ordenadas.slice(0,10);
 }
 
 function contaFrequencias(palavras) {
-    let frequencias = [];
+    let frequencias = {};
     for ( let i of palavras) {
         frequencias[i] = 0;
         for ( let j of palavras) {
@@ -36,4 +39,15 @@ function contaFrequencias(palavras) {
         }
     }
     return palavras;
+}
+
+function tiraPalavrasRuins(palavras) {
+    //const PALAVRAS_RUINS = new setInterval(["para", "uma", "nós"]); //tem muito mais!!
+    const palavrasBoas =[];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
